6d55ee7369a1a4c4c0a71a9028b2b37949f1ace4
Kristina Schmidt <kristina_schmidt21@web.de>
2025-11-27 09:35:00 +0100

    setup1

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import connectDatabase from "./db/connectDatabase.js";
 import startServer from "./server.js";
 
-const bootstrap= async(): Promise<void> => {
+const   bootstrap= async(): Promise<void> => {
     await connectDatabase();
     startServer();
 
